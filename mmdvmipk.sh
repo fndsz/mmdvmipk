bfa702e1b85e22bc7f896bba7ffb1eefb4f42fb0
fndsz <45010248+fndsz@users.noreply.github.com>
2024-08-26 17:12:24 +0800

    Update mmdvmipk.sh

--- mmdvmipk.sh
+++ mmdvmipk.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
 # MMDVM-OPENWRT
-make && sudo make install
-popd
-svn co https://github.com/qzyuebing/mmdvm-openwrt/YSFReflector-clients
-svn co https://github.com/qzyuebing/mmdvm-openwrt/dapnet-gateway
-svn co https://github.com/qzyuebing/mmdvm-openwrt/ircddb-gateway
-svn co https://github.com/qzyuebing/mmdvm-openwrt/libmmdvm
-svn co https://github.com/qzyuebing/mmdvm-openwrt/libwxwidgets
-svn co https://github.com/qzyuebing/mmdvm-openwrt/misc
-svn co https://github.com/qzyuebing/mmdvm-openwrt/mmdbm-cal
-svn co https://github.com/qzyuebing/mmdvm-openwrt/mmdvm-crossmode
-svn co https://github.com/qzyuebing/mmdvm-openwrt/mmdvm-host
-svn co https://github.com/qzyuebing/mmdvm-openwrt/mmdvm-luci
-svn co https://github.com/qzyuebing/mmdvm-openwrt/mmdvm
-svn co https://github.com/qzyuebing/mmdvm-openwrt/nxdn-clients
-svn co https://github.com/qzyuebing/mmdvm-openwrt/p25-clients
-svn co https://github.com/qzyuebing/mmdvm-openwrt/ysf-clients
-
-./scripts/feeds update -a
-./scripts/feeds install -a -f
