4425ee596b2a426369d9c280130858e389371ecc
fndsz <45010248+fndsz@users.noreply.github.com>
2025-05-07 17:49:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git mmdvm https://github.com/fndsz/mmdvm-op' feeds.conf.default
+#sed -i '$a src-git mmdvm https://github.com/fndsz/mmdvm-op' feeds.conf.default
 #sed -i '$a src-git mmdvmpatch https://github.com/fndsz/mmdvmpatch' feeds.conf.default
-#git clone https://github.com/fndsz/mmdvm-openwrt.git package/mmdvm/mmdvm-openwrt
+git clone https://github.com/fndsz/mmdvm-openwrt.git package/mmdvm/mmdvm-openwrt
 #git clone https://github.com/fndsz/mmdvmpatch.git package/mmdvm/mmdvm-openwrt
 #git clone https://github.com/fndsz/luci-app-netwizard.git package/lean/luci-app-netwizard
 git clone https://github.com/siwind/luci-app-wolplus.git package/lean/luci-app-wolplus
