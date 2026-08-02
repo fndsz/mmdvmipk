ff44701c676f20c4b3b36655331bfa144480c457
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-02 09:38:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,3 @@
 #sed -i '$a src-git mmdvmpatch https://github.com/fndsz/mmdvmpatch' feeds.conf.default
 git clone https://github.com/fndsz/mmdvm-op.git package/mmdvm/mmdvm-openwrt
 #git clone https://github.com/fndsz/mmdvmpatch.git package/mmdvm/mmdvm-openwrt
-#git clone https://github.com/fndsz/luci-app-oled.git package/lean/luci-app-oled
-#git clone https://github.com/siwind/luci-app-wolplus.git package/lean/luci-app-wolplus
-git clone https://github.com/fndsz/devtools-feeds.git package/lean/devtools-feeds
