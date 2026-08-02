9aa0261bc0a1a7cb2a51ff9b508841c45c59dbe0
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-03 01:46:15 +0800

    Update mmdvmipk.sh

--- mmdvmipk.sh
+++ mmdvmipk.sh
@@ -12,12 +12,10 @@ sed -i 's/ImmortalWrt/MMDVM/g' package/base-files/files/bin/config_generate
 sed -i 's/ssid=ImmortalWrt/ssid=MMDVM_4G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 开启MU-MIMO
-sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/Bootstrap_MMDVM/g' ./feeds/luci/collections/luci/Makefile#!/bin/bash
-
-# MMDVM-OPENWRT
